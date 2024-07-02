92f555c0d0528a843020fb0c8d1b3df4a67a713c
Aditya250401 <aa.21u10091@btech.nitdgp.ac.in>
2024-07-02 20:38:19 +0530

    added aws setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			aur yaha se shuru hota hai meri jindgi ka ek naya adhyay
+			aur is adhyay me sab kuch badhiya chal rha hai
 		</main>
 	)
 }
